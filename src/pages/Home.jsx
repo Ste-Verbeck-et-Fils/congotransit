0139084e4df9963f49dff1ae3420f7c3a8c281ad
JEAN-MARC VERBECK <jeanmarcverbeck.dev@gmail.com>
2026-04-27 21:06:33 +0200

    refactory: code et file refactor

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../styles/Home.css";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <div className="home-content">
+        <div className="logo-glow"></div>
+        <div className="spinner">
+          <div className="double-bounce1"></div>
+          <div className="double-bounce2"></div>
+        </div>
+        <h2 className="home-text">CongoTransit</h2>
+        <p className="home-subtext">Chargement de votre espace sécurisé...</p>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingPage;
